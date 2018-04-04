7a7ac058d366b2a04c77d5df4004668e90e91ec9
Rafael Labbe <32770652+ralabbe@users.noreply.github.com>
2018-04-04 09:51:21 -0400

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,17 @@
+
+
+<!-- Scripts -->
+	<!-- jQuery CDN -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<!-- jQuery UI CDN -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+	<!-- UI Kit & Icons -->
+	<script type="text/javascript" src="./js/uikit.min.js"></script>
+	<script type="text/javascript" src="./js/uikit-icons.min.js"></script>
+
+	<!-- Custom JS -->
+	<script type="text/javascript" src="./js/custom.js"></script>
+
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<?php
+//	include('connect.php'); // MySQL connection
+?>
+
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Untitled</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link rel="icon" type="./image/png" href="./img/favicon.png">
+
+	<!-- jQuery UI CSS -->
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	
+	<!-- UI Kit CSS -->
+	<link rel="stylesheet" href="./css/uikit.min.css">
+	
+	<!-- Custom CSS -->
+	<link type="text/css" rel="stylesheet" href="./css/custom.css"/>
+
+</head>
+<body>
